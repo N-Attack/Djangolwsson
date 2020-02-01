0c5823449c9c3eb663799f6fabedc92ab9e3ad21
coy5z9 <pobertbraun@yandex.ru>
2020-02-01 13:09:13 +0300

    heroku setup

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Question
 
 path = r"D:\Users\teacher\PycharmProjects\test_django\django_lesson\polls\templates\polls\index.html"
 
@@ -8,10 +8,7 @@ path = r"D:\Users\teacher\PycharmProjects\test_django\django_lesson\polls\templa
 def index(request):
     template = loader.get_template("polls/index.html")
     context = {
-        'comments': [
-            "самый худший фильм",
-            "автор прекрати!!!!"
-        ],
+            'items': Question.objects.order_by
     }
     return HttpResponse(template.render(context, request))
 
